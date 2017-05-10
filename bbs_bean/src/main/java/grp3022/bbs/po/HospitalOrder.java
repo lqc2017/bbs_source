70a50cef30197e07e0c9d48f29f7fe748eac00e3
全琛 <511976839@qq.com>
2017-05-10 09:00:15 +0800

    Signed-off-by: 全琛 <511976839@qq.com>

---
+++ bbs_bean/src/main/java/grp3022/bbs/po/HospitalOrder.java
@@ -0,0 +1,145 @@
+package grp3022.bbs.po;
+
+import java.util.Date;
+
+public class HospitalOrder {
+    private Long id;
+
+    private Short payWay;
+
+    private Date createTime;
+
+    private String describe;
+
+    private Short status;
+
+    private Double totalPrice;
+
+    private Date payTime;
+
+    private Long cashierId;
+
+    private Long patientId;
+
+    private Double subsidy;
+
+    private Double paidPrice;
+
+    private String payer;
+
+    private Short type;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Short getPayWay() {
+        return payWay;
+    }
+
+    public void setPayWay(Short payWay) {
+        this.payWay = payWay;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe == null ? null : describe.trim();
+    }
+
+    public Short getStatus() {
+        return status;
+    }
+
+    public void setStatus(Short status) {
+        this.status = status;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Date getPayTime() {
+        return payTime;
+    }
+
+    public void setPayTime(Date payTime) {
+        this.payTime = payTime;
+    }
+
+    public Long getCashierId() {
+        return cashierId;
+    }
+
+    public void setCashierId(Long cashierId) {
+        this.cashierId = cashierId;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public Double getSubsidy() {
+        return subsidy;
+    }
+
+    public void setSubsidy(Double subsidy) {
+        this.subsidy = subsidy;
+    }
+
+    public Double getPaidPrice() {
+        return paidPrice;
+    }
+
+    public void setPaidPrice(Double paidPrice) {
+        this.paidPrice = paidPrice;
+    }
+
+    public String getPayer() {
+        return payer;
+    }
+
+    public void setPayer(String payer) {
+        this.payer = payer == null ? null : payer.trim();
+    }
+
+    public Short getType() {
+        return type;
+    }
+
+    public void setType(Short type) {
+        this.type = type;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
